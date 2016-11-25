4331fc2e9761a433bb04114929a597696812b848
Catholiker T <Catholiker T>
2016-11-25 11:46:13 +0100

    commit

---
+++ assets/js/myjs.js
@@ -0,0 +1,13 @@
+//dropdown
+$('.ui.dropdown').dropdown();
+//sidebar
+$('.ui.sidebar').sidebar('attach events','.toc.item');
+
+});
+
+/*$(function(){
+    ${'a.item'}.click(function()){
+        ${'.item'}.removeClass{'active'};
+        ${this}.addClass{'active'};
+    })
+});*/
